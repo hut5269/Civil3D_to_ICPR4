81eb76e5cc20ae0619ef68c245c62545d64fb4a6
Jason Hutchinson <hut5269@gmail.com>
2023-08-23 20:10:01 -0400

    updates

--- helpers.py
+++ helpers.py
@@ -1,11 +1,10 @@
 from ipywidgets import FileUpload
 from IPython.display import display
 from IPython.display import clear_output
+from google.colab import files
 
 
 def file_upload(message):
-    clear_output()
-    print(message)
-    user_file = FileUpload(accept='.csv', multiple=False)
-    clear_output()
-    return '/content/Input/{0}'.format(next(iter(user_file)))
+    %cd /content/Input
+    uploaded = files.upload()
+    return uploaded
